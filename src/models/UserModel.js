19863f1032d0a1369581344759a1355d9063d9f7
GeorgeNeocleous <g.neocleous20@gmail.com>
2024-12-20 02:42:12 +1100

    feat(model): UserModel

---
+++ src/models/UserModel.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+
+const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique:true, 
+        minLength:3,
+        maxLength:20,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true, 
+        minLength:8,
+        trim: true
+    },
+});
+
+// Belongs in user collection and is based on the UserSchema
+const User = mongoose.model("User", UserSchema);
+
+module.exports = {
+    User
+}
