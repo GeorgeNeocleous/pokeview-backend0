ded7a5e78c7243bead1ed1abb43064276217e252
GeorgeNeocleous <g.neocleous20@gmail.com>
2024-12-21 02:34:32 +1100

    feat: added jwt testing route for prod in bruno

--- src/server.js
+++ src/server.js
@@ -14,30 +14,32 @@ app.get("/", (request, response) => {
     });
 });
 
-// Takes the username and password from a front end form
 app.post("/signup", async (request, response) => {
-    // check that a username and password are provided in request.body
-    let username = request.body.username;
-    let password = request.body.password;
+	// check that a username and password are provided in request.body 
+	let username = request.body.username;
+	let password = request.body.password;
 
-    // If no username or password is provided throws an error 400
-    if(!username || !password){
-        response.status(400).json({
-            message:"Incorrect or missing sign-up credentials provided."
-        });
-    }
-    // make a user in the DB using the username and password
-    let newUser = await User.create({username: username, password: password});
-    // make a JWT based on the username and userID
-    let newJwt = generateJWT(newUser.id, newUser.username);
-    // return the JWT
-    response.json({
-        jwt:newJwt,
-        user: {
-            id: newUser.id,
-            username: newUser.username
-        }
-    });
+	if (!username || !password) {
+		response.status(400).json({
+			message:"Incorrect or missing sign-up credentials provided."
+		});
+	}
+
+
+	// make a user in the DB using the username and password
+	let newUser = await User.create({username: username, password: password});
+
+	// make a JWT based on the username and userID 
+	let newJwt = generateJWT(newUser.id, newUser.username);
+
+	// return the JWT 
+	response.json({
+		jwt: newJwt,
+		user: {
+			id: newUser.id,
+			username: newUser.username
+		}
+	});
 });
 
 app.get("/authenticationRoute", validateUserAuth, (request, response) => {
